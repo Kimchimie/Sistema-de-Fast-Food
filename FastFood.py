277fb6ebbe077c28a854d4c6d356096cbe82c457
Kimchimie <124799218+Kimchimie@users.noreply.github.com>
2023-02-08 11:33:25 -0300

    Add files via upload

---
+++ FastFood.py
@@ -0,0 +1,32 @@
+print('1- Hamburguer - R$10,00')
+print('2- Batata Frita - R$5,00')
+print('3- Refrigerante - R$4,00')
+print('4- Sorvete - R$2,00')
+
+opcao = int(input('Digite o numero da opção desejada: '))
+quantidade = int(input('Digite a quantidade desejada: '))
+nome = input('Digite o nome do cliente: ')
+
+if opcao == 1:
+    print('Hamburguer sendo preparado para ',nome)
+    print('Tempo de espera de 15 minutos')
+    total = quantidade * 10
+    print('Total: R$', total)
+
+if opcao == 2: 
+    print('Batata Frita sendo preparada para: ',nome)
+    print('Tempo de espera de 10 minutos')
+    total = quantidade * 5
+    print('Total: R$',total)
+
+if opcao == 3:
+    print('Refrigerante sendo preparado para: ',nome)
+    print('Tempo de espera de 2 minutos')
+    total = quantidade * 4
+    print('Total: R$',total)
+
+if opcao == 4:
+    print('Sorvete sendo preparado para: ',nome)
+    print('Tempo de espera de 5 minutos')
+    total = quantidade * 2
+    print('Total: R$',total)
